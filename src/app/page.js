23d4167f0b79312c783d97ac2fd0cd9a90c7b32d
heerokj <jungheerok@gmail.com>
2025-04-02 02:50:12 +0900

    영역 나누기

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,31 @@
+import AboutMe from "@/components/AboutMe";
+import Career from "@/components/Career";
+import Contact from "@/components/Contact";
+import Main from "@/components/Main";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+
 export default function Home() {
   return (
     <>
-      <section>home</section>
-      <section>about me</section>
-      <section>skills</section>
-      <section>project</section>
-      <section>career</section>
-      <section>contact</section>
+      <section className="bg-amber-100 min-h-screen">
+        <Main />
+      </section>
+      <section className="bg-amber-200 min-h-screen">
+        <AboutMe />
+      </section>
+      <section className="bg-amber-300 min-h-screen">
+        <Skills />
+      </section>
+      <section className="bg-amber-400 min-h-screen">
+        <Projects />
+      </section>
+      <section className="bg-amber-500 min-h-screen">
+        <Career />
+      </section>
+      <section className="bg-amber-600 min-h-screen">
+        <Contact />
+      </section>
     </>
   );
 }
