af9f7e5a5e089e62b83d5c9c8a6897cc1cb7524c
heerokj <jungheerok@gmail.com>
2025-04-02 17:36:47 +0900

    Header 영역 + 스타일 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-between items-center fixed top-0 w-full py-8 px-16">
+      <div className="header-logo">
+        <h1 className="font-bold text-3xl font-logo">
+          <a href="#">Jungheerok</a>
+        </h1>
+      </div>
+      <nav>
+        <ul className="header-menu flex gap-6 text-2xl font-bold px-8 py-4 bg-amber-50 rounded-full shadow-md">
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#career">Career</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
---
+++ src/components/ProfilePhoto.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProfilePhoto() {
+  return <div>ProfilePhoto</div>;
+}
