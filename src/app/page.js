ec629e2ae95505071743cf880e46ec51488c56d3
heerokj <jungheerok@gmail.com>
2025-04-07 20:10:13 +0900

    style : 물결 애니메이션 바탕화면 적용

--- src/app/page.js
+++ src/app/page.js
@@ -8,22 +8,48 @@ import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <>
-      <section id="main" className="bg-amber-100 min-h-screen">
+      <section
+        id="main"
+        className="min-h-screen flex items-center justify-center"
+      >
+        {/* 물결 배경 */}
+        <div className="absolute top-0 left-0 w-full h-full -z-10">
+          <div className="wave -one absolute top-[20%] left-[50%] bg-[#f8cdda] w-[200vw] h-[225vw] ml-[-100vw] mt-[-196vw] rounded-[40%] opacity-25 animate-wave-one"></div>
+          <div className="wave -two absolute top-[20%] left-[50%] bg-[#fbd3e9] w-[200vw] h-[225vw] ml-[-100vw] mt-[-196vw] rounded-[40%] opacity-25 animate-wave-two"></div>
+          <div className="wave -three absolute top-[20%] left-[50%] bg-[#fceabb] w-[200vw] h-[225vw] ml-[-100vw] mt-[-196vw] rounded-[40%] opacity-25 animate-wave-three"></div>
+          <div className="wave -three absolute top-[20%] left-[50%] bg-[#d291bc] w-[200vw] h-[225vw] ml-[-100vw] mt-[-196vw] rounded-[40%] opacity-25 animate-wave-three"></div>
+        </div>
+
         <Main />
       </section>
-      <section id="about" className="bg-amber-200 min-h-screen">
+      <section
+        id="about"
+        className="bg-amber-200 min-h-screen flex items-center justify-center"
+      >
         <AboutMe />
       </section>
-      <section id="skills" className="bg-amber-300 min-h-screen">
+      <section
+        id="skills"
+        className="bg-amber-300 min-h-screen flex items-center justify-center"
+      >
         <Skills />
       </section>
-      <section id="projects" className="bg-amber-400 min-h-screen">
+      <section
+        id="projects"
+        className="bg-amber-100 min-h-screen flex items-center justify-center"
+      >
         <Projects />
       </section>
-      <section id="career" className="bg-amber-500 min-h-screen">
+      <section
+        id="career"
+        className="bg-amber-200 min-h-screen flex items-center justify-center"
+      >
         <Career />
       </section>
-      <section id="contact" className="bg-amber-600 min-h-screen">
+      <section
+        id="contact"
+        className="bg-amber-300 min-h-screen flex flex-col justify-center"
+      >
         <Contact />
       </section>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center fixed top-0 w-full py-6 px-[80px]">
+    <header className="flex justify-between items-center fixed top-0 w-full py-6 px-[80px] z-10">
       <div className="header-logo">
         <h1 className="font-bold text-3xl font-logo">
           <a href="#">Jungheerok</a>
         </h1>
       </div>
       <nav>
-        <ul className="header-menu flex gap-6 text-2xl font-bold px-8 py-4 bg-amber-50 rounded-full shadow-md ">
+        <ul className="header-menu flex gap-6 text-2xl font-bold px-8 py-4 bg-gray-100 rounded-full shadow-md ">
+          <li>
+            <a href="#main">home</a>
+          </li>
           <li>
             <a href="#about">About Me</a>
           </li>
