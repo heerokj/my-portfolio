157badeba17868a9a869e30f5eacf71cde7dd8b2
heerokj <jungheerok@gmail.com>
2025-04-08 15:49:32 +0900

    fix : 메타데이터 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,22 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "jungheerok 포트폴리오",
+  title: "정희록 프론트엔드 포트폴리오",
   description: "정희록의 포트폴리오 입니다☺️",
   keywords: "Next.js, 웹 개발, 포트폴리오, 프론트엔드",
+  openGraph: {
+    type: "website",
+    url: "https://jungheerok.vercel.app/",
+    title: "정희록 프론트엔드 포트폴리오",
+    description: "안녕하세요! 정희록의 포트폴리오 사이트입니다.☺️",
+    siteName: "정희록 프론트엔드 포트폴리오",
+    images: [
+      {
+        url: "/images/heerok-character.png",
+        alt: "jungheerok image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
